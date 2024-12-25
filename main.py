9f283beca28256f55e560b928ec4ab731230deb0
Vitaly L <vitalyl@yandex.ru>
2024-12-25 22:53:25 +0300

    add settings from env

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+from project_manager.config import settings
 
 
 app = FastAPI()
@@ -10,4 +11,7 @@ async def hello():
     return "hello world"
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run("main:app",
+                host=settings.server.host,
+                port=settings.server.port,
+                reload=settings.server.reload)
---
+++ project_manager/config.py
@@ -0,0 +1,28 @@
+from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
+from pydantic import BaseModel
+from pydantic import PostgresDsn
+
+
+class ServerSettings(BaseModel):
+    host: str
+    port: str
+    reload: bool
+
+
+class DbSettings(BaseModel):
+    url: PostgresDsn
+    echo: bool
+
+
+class Settings(BaseSettings):
+    db: DbSettings
+    server: ServerSettings
+    model_config = SettingsConfigDict(case_sensitive=False,
+                                      env_file=".env",
+                                      env_nested_delimiter="__",
+                                      env_prefix="APP_CONFIG__")
+
+
+settings = Settings()
+print(settings)
