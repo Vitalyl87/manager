626186eb059bb9449621f95d4c6188401c89b388
Vitaly L <vitalyl@yandex.ru>
2025-01-05 12:16:50 +0300

    add task post

--- project_manager/task/__init__.py
+++ project_manager/task/__init__.py
@@ -3,11 +3,42 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from project_manager.config import settings
 from project_manager.db_helper import dp_helper
-from project_manager.task.dao import get_all_tasks
+from project_manager.task.dao import TaskDao
+from project_manager.task.schemas import TaskCreate, TaskRead
+from project_manager.task.status import Status
 
 router = APIRouter(prefix=settings.prefix.task_prefix)
 
 
 @router.get("/")
-async def get_tasks(session: AsyncSession = Depends(dp_helper.get_session)):
-    return await get_all_tasks(session)
+async def get_tasks(
+    session: AsyncSession = Depends(dp_helper.get_session),
+) -> list[TaskRead]:
+    return await TaskDao.get_all(session)
+
+
+@router.get("/{project_id}")
+async def get_task_by_project_id(
+    project_id: int,
+    status: Status = None,
+    session: AsyncSession = Depends(dp_helper.get_session),
+) -> list[TaskRead]:
+    if status is None:
+        print("dadadadada")
+        return await TaskDao.get_task_by_project_id(
+            session=session, project_id=project_id
+        )
+    return await TaskDao.get_task_by_project_id(
+        session=session, project_id=project_id, status=status
+    )
+
+
+@router.post("/", status_code=200)
+async def add_task_for_project(
+    task_data: TaskCreate,
+    session: AsyncSession = Depends(dp_helper.get_session),
+) -> None:
+    await TaskDao.create_task_for_project_id(session=session, task=task_data)
+    return {
+        "message": f"Task for project (id={task_data.project_id}) added successfully"
+    }
--- project_manager/task/dao.py
+++ project_manager/task/dao.py
@@ -1,10 +1,37 @@
+from fastapi import HTTPException
 from sqlalchemy import Select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from project_manager.base_dao import BaseDao
 from project_manager.task.models import Task
+from project_manager.task.schemas import TaskCreate, TaskRead
 
 
-async def get_all_tasks(session: AsyncSession) -> list[Task]:
-    stmt = Select(Task).order_by(Task.id)
-    result = await session.scalars(stmt)
-    return result.all()
+class TaskDao(BaseDao):
+    model = Task
+
+    @classmethod
+    async def get_task_by_project_id(
+        cls, session: AsyncSession, project_id: int, **filter_by
+    ) -> list[TaskRead]:
+        print("DAO!")
+        print(filter_by)
+        stmt = (
+            Select(cls.model)
+            .where(cls.model.project_id == project_id)
+            .filter_by(**filter_by)
+            .order_by(cls.model.id)
+        )
+        result = await session.scalars(stmt)
+        return result.all()
+
+    @classmethod
+    async def create_task_for_project_id(cls, session: AsyncSession, task: TaskCreate):
+        data = task.model_dump()
+        new_task = Task(**data)
+        session.add(new_task)
+        try:
+            await session.commit()
+        except Exception as ex:
+            await session.rollback()
+            raise HTTPException(status_code=500, detail=str(ex.orig))
